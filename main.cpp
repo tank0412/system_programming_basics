2b4c6c2d4bdec085468a5c42bcc490fc23dc1a04
Roman <romabukhtiarov@gmail.com>
2020-01-12 22:56:29 +0300

    Impl get number of buffer lines from -n arg

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "stdio.h"
 #include <string.h>
 #include <unistd.h>
+#include <ctype.h>
 
 using namespace std;
 
@@ -9,9 +10,15 @@ int MAX = 900;
 
 int main(int argc, char** argv) {
     FILE *fp;
+    int maxBufLines = 10;
     printf("Lab2 Bukhtiarov P3418 ");
     for(int i =1; i < argc; i++) { // ignore 0 argument whith exec path
     printf("Argument %d: %s\n", i, argv[i]);
+    if(strcmp(argv[i], "-n") == 0) {
+        if(*argv[i+1] >= '0' && *argv[i+1] <= '9' ) {
+        maxBufLines = atoi(argv[i+1]);
+        }
+    }
     }
     char cwd[MAX];
     if (getcwd(cwd, sizeof(cwd)) != NULL) {
@@ -20,10 +27,14 @@ int main(int argc, char** argv) {
     
     char* fullPath;
     char slash[2] = {'/'};
-    fullPath = (char*)malloc(strlen(cwd)+strlen(argv[1]) + strlen(slash)); /* make space for the new string (should check the return value ...) */
+    int fileNameIndex = 0;
+    while(strstr(argv[fileNameIndex], ".txt") == NULL) {
+    fileNameIndex++;
+    }
+    fullPath = (char*)malloc(strlen(cwd)+strlen(argv[fileNameIndex]) + strlen(slash)); /* make space for the new string (should check the return value ...) */
     strcpy(fullPath, cwd); /* copy name into the new var */
     strcat(fullPath, slash);
-    strcat(fullPath, argv[1]); /* add the extension */
+    strcat(fullPath, argv[fileNameIndex]); /* add the extension */
     
     /*
     printf(slash);
@@ -43,7 +54,7 @@ int main(int argc, char** argv) {
             if(buffer[length -1] == '\n' ) {
             if(strcmp(buffer, "") != 0) {
             countOfLines++;
-            if(countOfLines >= 10) {
+            if(countOfLines > maxBufLines) {
                 continue;
             }
             printf("%s", buffer);
